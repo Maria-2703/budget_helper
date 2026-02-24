6885b58ca9979a4898276ddc10027c4b7e716c59
Maria-2703 <mariaparraramos@yahoo.com>
2026-02-24 23:06:34 +0100

    update classification system

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ load_dotenv()
 app = Flask(__name__)
 
 api_key = os.getenv("COHERE_API_KEY")
+user = os.getenv("MONGO_USERNAME")
+pw = os.getenv("MONGO_PASSWORD")
+
 co = cohere.ClientV2(api_key=api_key)
     
-def classify(data):
-    transactions = data.to_dict(orient="records")
+def classify_co(unclassified):
+    transactions = unclassified.to_dict(orient="records")
     prompt = f"""
     You are a financial transaction classification assistant.
     Your task is to analyze the bank statement uploaded by the user and classify EACH transaction into ONE of the following categories:
@@ -82,27 +85,77 @@ def classify(data):
     result = response.message.content[0].text
     return result
 
+def classify_kw(merged_df):
+    # read json with keywords
+    with open("keywords.json") as file:
+        keywords = json.load(file)
+
+    # df to dict
+    merged_df.dropna(how="all", inplace=True)
+    transactions = merged_df.to_dict(orient="records")
+
+    # for descriptions that can be classified with keywords
+    classified = []
+    unclassified = []
+
+    # check if keyword is in any description
+    for item in transactions:
+        desc = item.get("Description", "").lower()
+        found = False
+        for category, kws in keywords.items():
+            for kw in kws:
+                if kw.lower() in desc:
+                    item["Category"] = category   # if so, add the category
+                    classified.append(item)
+                    found = True
+                    break
+            if found:
+                break
+        if not found:
+            unclassified.append(item)  # the unclassified ones
+
+    # finish classifying with cohere
+    if unclassified:
+        classified_co_json = classify_co(pd.DataFrame(unclassified))
+        classified_co = json.loads(classified_co_json)
+        classified += classified_co
+
+    
+    result = pd.DataFrame(classified)
+
+    return result
+
 @app.route("/budgeting", methods=["GET","POST"])
 def budgeting():
     if request.method == "POST":
+
         # if there isn't a file
-        if "file" not in request.files:
-            return {"error" : "No file uploaded"}
+        if "files" not in request.files:
+            return {"error" : "No files uploaded"}
 
-        # get file user uploads
-        file = request.files["file"]
-        data = pd.read_csv(file)
+        # get files user uploads
+        uploaded_files = request.files.getlist("files")
 
-        # send data to ai
-        classified = classify(data)
-        print(classified)
+        all_data = []
+        for file in uploaded_files:
+            data = pd.read_csv(file)
+            data.dropna(how="all", inplace=True)
+            all_data.append(data)
+        
+        merged_df = pd.concat(all_data, ignore_index=True)
+        merged_df = pd.DataFrame(merged_df)
 
-        # json to df
-        classified = json.loads(classified)
-        data_result = pd.DataFrame(classified)
+        # classify expenses
+        result = classify_kw(merged_df)
+        
+        # final df
+        data_result = pd.DataFrame(result)
+        data_result.dropna(how="all", inplace=True)
 
         return render_template("home.html", table=data_result.to_html(index=False))
+
     return render_template("home.html", table=None)
+
 
 if __name__ == "__main__":
     app.run(debug = True, host = "localhost", port  = 5000)
